d2ab6613dc30fe3e1d976fbd7e665788e1a6a875
PATATESUPREME <patatesup@gmail.com>
2017-01-31 16:03:54 +0100

    Add the entities

---
+++ src/RestaurantBundle/Entity/Menu.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace RestaurantBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Menu
+ *
+ * @ORM\Table(name="menu")
+ * @ORM\Entity(repositoryClass="RestaurantBundle\Repository\MenuRepository")
+ */
+class Menu
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Dish", mappedBy="menus")
+     */
+    private $dishes;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="string", length=255)
+     */
+    private $price;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="displayOrder", type="integer")
+     */
+    private $displayOrder;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Menu
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Menu
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set displayOrder
+     *
+     * @param integer $displayOrder
+     *
+     * @return Menu
+     */
+    public function setDisplayOrder($displayOrder)
+    {
+        $this->displayOrder = $displayOrder;
+
+        return $this;
+    }
+
+    /**
+     * Get displayOrder
+     *
+     * @return int
+     */
+    public function getDisplayOrder()
+    {
+        return $this->displayOrder;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return Menu
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Menu
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->dishes = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add dish
+     *
+     * @param \RestaurantBundle\Entity\Dish $dish
+     *
+     * @return Menu
+     */
+    public function addDish(\RestaurantBundle\Entity\Dish $dish)
+    {
+        $this->dishes[] = $dish;
+
+        return $this;
+    }
+
+    /**
+     * Remove dish
+     *
+     * @param \RestaurantBundle\Entity\Dish $dish
+     */
+    public function removeDish(\RestaurantBundle\Entity\Dish $dish)
+    {
+        $this->dishes->removeElement($dish);
+    }
+
+    /**
+     * Get dishes
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDishes()
+    {
+        return $this->dishes;
+    }
+}
